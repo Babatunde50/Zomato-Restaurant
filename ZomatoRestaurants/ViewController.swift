599c005670f5c29e685132a96af7dc686eefe5c0
babatunde50 <babatundeololade50@gmail.com>
2020-12-16 21:29:49 +0100

    Initial Commit

---
+++ ZomatoRestaurants/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ZomatoRestaurants
+//
+//  Created by Tunde Ola on 12/16/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
